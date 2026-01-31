6b1ac5aa7d67e8ead5184120bdb73e71abd94b10
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-02-01 00:28:37 +0600

    updated card carousel

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const products = [
-  { id: 1, name: "Chair Modern", category: "chair", price: 120, image: "chair1.png" },
-  { id: 2, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
-  { id: 3, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
-  { id: 4, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
-   { id: 5, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
-  { id: 6, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
-  { id: 7, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
-  { id: 8, name: "Night Lamp", category: "lamp", price: 90, image: "lamp1.jpg" },
-   { id: 9, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
-  { id: 10, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
+  { id: 1, name: "Chair",subname: "Sakarias Armchair", category: "chair", price: 120, image: "chair1.png",rating: "stars.png" },
+  { id: 2, name: "Chair",subname: "Baltsar Chair", category: "chair", price: 150, image: "chair2.png",rating:"stars.png" },
+  { id: 3, name: "Chair",subname: "Anjay Chair", category: "chair", price: 600, image: "chair3.png",rating:"stars.png" },
+   { id: 4, name: "Chair",subname: "Nyantuy Chair", category: "chair", price: 600, image: "chair4.png",rating:"stars.png" },
+  { id: 5, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png",rating:"stars.png" },
+   { id: 6, name: "Chair",subname: "Anjay Chair", category: "chair", price: 150, image: "chair2.png" },
+  { id: 7, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
+  { id: 8, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
+  { id: 9, name: "Night Lamp", category: "lamp", price: 90, image: "lamp1.jpg" },
+   { id: 10, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
+ 
   { id: 11, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
    { id: 12, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
   { id: 13, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
@@ -39,10 +40,12 @@ function renderProducts(list) {
     chunk.forEach(p => {
       slide += `
         <div class="col-6 col-md-3 col-lg-3">
-          <div class="card h-100 text-center">
+          <div class="card item text-start">
             <img src="images/${p.image}" class="card-img-top">
             <div class="card-body">
-              <h6>${p.name}</h6>
+              <h6 class="text-start">${p.name}</h6>
+              <h2 class="text-start txt9">${p.subname}</h2>
+              <img src="images/${p.rating}"class="text-start">
               <p>$${p.price}</p>
               <button class="btn btn-sm btn-warning" onclick="addToCart()">Add to cart</button>
             </div>
