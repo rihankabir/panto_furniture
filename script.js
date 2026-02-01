ea9b26706e07f15825ae98cacf67ec516f196a81
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-02-01 22:40:06 +0600

    updated product card section

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const products = [
-  { id: 1, name: "Chair",subname: "Sakarias Armchair", category: "chair", price: 120, image: "chair1.png",rating: "stars.png" },
-  { id: 2, name: "Chair",subname: "Baltsar Chair", category: "chair", price: 150, image: "chair2.png",rating:"stars.png" },
-  { id: 3, name: "Chair",subname: "Anjay Chair", category: "chair", price: 600, image: "chair3.png",rating:"stars.png" },
-   { id: 4, name: "Chair",subname: "Nyantuy Chair", category: "chair", price: 600, image: "chair4.png",rating:"stars.png" },
+  { id: 1, name: "Chair",subname: "Sakarias Armchair", category: "chair", price: 392, image: "chair1.png",rating: "stars.png" },
+  { id: 2, name: "Chair",subname: "Baltsar Chair", category: "chair", price: 299, image: "chair2.png",rating:"stars.png" },
+  { id: 3, name: "Chair",subname: "Anjay Chair", category: "chair", price: 519, image: "chair3.png",rating:"stars.png" },
+   { id: 4, name: "Chair",subname: "Nyantuy Chair", category: "chair", price: 921, image: "chair4.png",rating:"stars.png" },
   { id: 5, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png",rating:"stars.png" },
    { id: 6, name: "Chair",subname: "Anjay Chair", category: "chair", price: 150, image: "chair2.png" },
-  { id: 7, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
-  { id: 8, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
-  { id: 9, name: "Night Lamp", category: "lamp", price: 90, image: "lamp1.jpg" },
-   { id: 10, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
+  { id: 7, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png",rating:"stars.png" },
+  { id: 8, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png",rating:"stars.png" },
+  { id: 9, name: "Night Lamp", category: "lamp", price: 90, image: "lamp1.jpg",rating:"stars.png" },
+   { id: 10, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png",rating:"stars.png" },
  
-  { id: 11, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
-   { id: 12, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
-  { id: 13, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
-  { id: 14, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
-   { id: 15, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png" },
-  { id: 16, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png" },
-  { id: 17, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png" },
+  { id: 11, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png",rating:"stars.png" },
+   { id: 12, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png",rating:"stars.png" },
+  { id: 13, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png",rating:"stars.png" },
+  { id: 14, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png",rating:"stars.png" },
+   { id: 15, name: "Chair Classic", category: "chair", price: 150, image: "chair2.png",rating:"stars.png" },
+  { id: 16, name: "Sofa Comfort", category: "sofa", price: 600, image: "chair3.png",rating:"stars.png" },
+  { id: 17, name: "Luxury Bed", category: "bed", price: 900, image: "chair4.png",rating:"stars.png" },
 ];
 
 let cartCount = 0;
@@ -41,13 +41,15 @@ function renderProducts(list) {
       slide += `
         <div class="col-6 col-md-3 col-lg-3">
           <div class="card item text-start">
-            <img src="images/${p.image}" class="card-img-top">
-            <div class="card-body">
+            <img src="images/${p.image}" class="card-img-top context3">
+            <div class="card-body mt-1">
               <h6 class="text-start">${p.name}</h6>
               <h2 class="text-start txt9">${p.subname}</h2>
               <img src="images/${p.rating}"class="text-start">
-              <p>$${p.price}</p>
-              <button class="btn btn-sm btn-warning" onclick="addToCart()">Add to cart</button>
+              <div class="d-flex align-items-center justify-content-between context mt-5">
+              <p class="mt-4 context1">$${p.price}</p>
+              <button class="btn btn-sm btn" onclick="addToCart()"><img src="images/cart_button.png"></button>
+              </div>
             </div>
           </div>
         </div>
