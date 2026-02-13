d76666bb924f081b1d0bf9a91147a9a2c772833b
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-02-13 18:22:58 +0600

    committed

--- script.js
+++ script.js
@@ -61,12 +61,12 @@ function renderProducts(list) {
 
     let slideHTML = `
       <div class="carousel-item ${i === 0 ? "active" : ""}">
-        <div class="row g-4 align-items-center">
+        <div class="row g-5 justify-content-center align-items-center">
     `;
 
     chunk.forEach(p => {
       slideHTML += `
-        <div class=" col-xl-3 col-12 col-sm-6 col-md-5 col-lg-3">
+        <div class="col-xl-3 col-12 col-sm-6 col-md-5 col-lg-3">
           <div class="card item text-start h-100">
             <img src="images/${p.image}" class="card-img-top context3" alt="${p.name}">
 
