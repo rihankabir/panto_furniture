464af6f6b0110e72447a8c86eaf9db4f533f7e49
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-02-14 15:18:18 +0600

    comitted

--- script.js
+++ script.js
@@ -148,3 +148,29 @@ window.addEventListener("resize", () => {
    INITIAL LOAD
 ======================= */
 loadProducts();
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
